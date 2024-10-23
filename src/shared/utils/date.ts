3a79e8159399c38d4f9bcf8f27aae848c37b9b44
gabriel-waltmann <gabrielwaltmann@gmail.com>
2024-10-23 14:27:41 -0300

    feat: add date util

--- src/shared/utils/date.ts
+++ src/shared/utils/date.ts
@@ -1,11 +1,26 @@
-export const toBR = (date: Date): string => {
-  return date.toLocaleString('pt-BR', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    hour12: false, // Use 24-hour format
-  });
-};
+import moment from 'moment-timezone';
+
+interface IDateUtils {
+  BRtoISO(date?: string, tz?: string): string | null;
+}
+
+export default class DateUtils implements IDateUtils {
+
+  BRtoISO(date_br?: string, tz = 'America/Sao_Paulo'): string | null {
+    if (!date_br || (new Date(date_br)).toString() === 'Invalid Date') {
+      return null;
+    }
+
+    // Format the moment object as an ISO date string
+    const date = moment(date_br, "YYYY-MM-DD HH:mm:ss");
+
+    // Set the timezone
+    date.tz(tz);
+    
+    // Convert to UTC+0
+    const dtUtc = date.utc();
+    
+    // Format the moment object as an ISO date string
+    return dtUtc.toISOString();
+  }
+}
